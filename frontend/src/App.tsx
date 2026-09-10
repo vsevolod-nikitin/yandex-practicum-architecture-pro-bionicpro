8d8624b5e26f6b03b6002a79e9242e7657c1e892
Budun4eg <budun4eg@gmail.com>
2026-09-10 20:22:24 +0300

    Задача 1.2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,10 +10,11 @@ const keycloakConfig: KeycloakConfig = {
 };
 
 const keycloak = new Keycloak(keycloakConfig);
+const initOptions = { pkceMethod: "S256" };
 
 const App: React.FC = () => {
   return (
-    <ReactKeycloakProvider authClient={keycloak}>
+    <ReactKeycloakProvider authClient={keycloak} initOptions={initOptions}>
       <div className="App">
         <ReportPage />
       </div>
